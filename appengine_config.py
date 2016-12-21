144f5c1766a0e8e1165fc507ee0108b7433352ef
Vincent Buscarello <vince.buscarello@gmail.com>
2016-12-21 16:58:14 -0700

    edited yaml

--- appengine_config.py
+++ appengine_config.py
@@ -15,4 +15,4 @@ from google.appengine.ext import vendor
 # insert `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions.
-vendor.add('nibmle\lib')
+vendor.add('.\\nibmle\\lib')
