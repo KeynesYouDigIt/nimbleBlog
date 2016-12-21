ccebd9c2ecab6a8485070e60f7dbe61990e8b9ab
Vincent Buscarello <vince.buscarello@gmail.com>
2016-12-21 16:54:26 -0700

    edited yaml

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,13 @@
 """`appengine_config` gets loaded when starting a new application instance."""
 
+
+import sys
+import os.path
+
+# add `lib` subdirectory to `sys.path`, so our `main` module can load
+# third-party libraries.
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'nibmle\lib'))
+
 print 'running app  config yaya!'
 
 from google.appengine.ext import vendor
@@ -7,7 +15,4 @@ from google.appengine.ext import vendor
 # insert `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions.
-try:
-    vendor.add('nibmle\lib')
-except as err:
-    print err
+vendor.add('nibmle\lib')
