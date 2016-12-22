c7c9699c9d56f8a259108afba52288fae4083fd4
Vincent Buscarello <vince.buscarello@gmail.com>
2016-12-22 10:58:57 -0700

    fixing deps

--- appengine_config.py
+++ appengine_config.py
@@ -1,28 +1,9 @@
 """`appengine_config` gets loaded when starting a new application instance."""
 
-
-# import sys
-# import os.path
-
-# # add `lib` subdirectory to `sys.path`, so our `main` module can load
-# # third-party libraries.
-# sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'nibmle\lib'))
-
 print 'running app  config yaya!'
 
-from google.appengine.ext import vendor
-# import os
-# import sys
-
-# import vendor
-# insert `lib` as a site directory so our `main` module can load
-# third-party libraries, and override built-ins with newer
-# versions.
-
-# sys.path.insert(0,'./lib')
-# print sys.path
-
-from google.appengine.ext import vendor
-print dir(vendor)
+import vendor
 vendor.add('lib')
 print 'I am the line after adding lib, it should have worked'
+import os
+print os.getcwd()
