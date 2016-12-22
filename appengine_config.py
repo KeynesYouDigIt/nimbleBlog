5fb9aaab9aad27bd97e564e946ca925e1e6da2e9
Vincent Buscarello <vince.buscarello@gmail.com>
2016-12-21 17:33:40 -0700

    edited yaml

--- appengine_config.py
+++ appengine_config.py
@@ -11,9 +11,14 @@
 print 'running app  config yaya!'
 
 from google.appengine.ext import vendor
+import os
 # import vendor
 # insert `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions.
+print os.path.realpath(__file__)
+
+print os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+
 vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 #vendor.add('lib')
